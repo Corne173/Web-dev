a29e40ee0d4f6b4b5fb10ab04cf40a405501fac2
Neo Modisane <neo@rdtek.co.za>
2020-06-01 14:37:29 +0200

    Just the formatting I believe we should go for

--- JS-Basics/index.js
+++ JS-Basics/index.js
@@ -52,4 +52,12 @@ function Stopwatch() {
   };
 }
 
+function page(page) {
+  var i;
+  var x = document.getElementsByClassName("page");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(page).style.display = "block";
+}
 const sw = new Stopwatch();
